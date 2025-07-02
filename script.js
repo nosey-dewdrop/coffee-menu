2425837a289bd4d7a94d59250891abdac6f53edc
nosey-dewdrop <damummyphus@gmail.com>
2025-07-03 00:35:12 +0300

    i bought myself a ring today

---
+++ script.js
@@ -0,0 +1,15 @@
+/*
+1. stepTitle
+
+2. no id. 
+      2.1 coffeeVisual
+      2.2 ingredientsList
+      2.3 coffeeName
+      2.4 suggestions    
+      2.5 priceDisplay   
+
+3. optionsGrid
+4. no id.
+      4.1 resetBtn onclick="resetCoffee()"
+      4.2 finishBtn onclick="finishCoffee()"
+*/
